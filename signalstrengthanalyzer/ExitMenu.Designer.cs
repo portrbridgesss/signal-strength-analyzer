038803f23d66af101e70acd0f0f4431ca9043c75
RenjaizeCollinDinaml <RenjaizeCollinDinaml@SMU-0068>
2025-12-02 11:25:24 +0800

    Added Exit Menu

---
+++ signalstrengthanalyzer/ExitMenu.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace signalstrengthanalyzer
+{
+    partial class ExitMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonHighlight;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(459, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Do You Want to Exit The Program?";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(35, 67);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 86);
+            button1.TabIndex = 1;
+            button1.Text = "YES";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(266, 67);
+            button2.Name = "button2";
+            button2.Size = new Size(178, 86);
+            button2.TabIndex = 2;
+            button2.Text = "NO";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // ExitMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(483, 175);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "ExitMenu";
+            Text = "ExitMenu";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+    }
+}
